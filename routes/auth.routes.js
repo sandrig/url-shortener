86c36f0bba9416be212e24ffddeea428d797dd62
sandrig <sandrig.dev@gmail.com>
2019-12-29 20:00:50 +0300

    feat: add register validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,28 @@
 const { Router } = require('express');
 const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
 
 // /api/auth/register
-router.post('/register', async (req, res) => {
+router.post(
+  '/register',
+  [
+    check('email', 'Некорректный email').isEmail(),
+    check('password', 'Минимальная длина пароля 6 символов')
+      .isLength({ min: 6 }),
+  ],
+  async (req, res) => {
   try {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Некорректные данные при регистрации'
+      })
+    }
+
     const { email, password } = req.body;
 
     const candidate = await User.findOne({ email });
@@ -19,10 +36,7 @@ router.post('/register', async (req, res) => {
 
     await user.save();
 
-    req.status(201).json({ message: 'Пользователь создан' })
-
-
-
+    req.status(201).json({ message: 'Пользователь создан' });
 
   } catch (e) {
     res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
@@ -35,5 +49,3 @@ router.post('/login', async (req, res) => {
 });
 
 module.exports = router;
-
-
