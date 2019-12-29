46077bb763ca07c6fd330cff3e1cc8b7ae2f6712
sandrig <sandrig.dev@gmail.com>
2019-12-29 20:17:16 +0300

    feat: add login validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -44,8 +44,40 @@ router.post(
 });
 
 // /api/auth/login
-router.post('/login', async (req, res) => {
+router.post(
+  '/login',
+  [
+    check('email', 'Введите корректный email').normalizeEmail().isEmail(),
+    check('password', 'Введите пароль').exists
+  ],
+  async (req, res) => {
+  try {
+    const errors = validationResult(req);
 
+    if(!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+        message: 'Некорректные данные при входе в систему'
+      })
+    }
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user) {
+      return res.status(400).json({ message: 'Поо=льзователь не найден' });
+    }
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if(!isMatch) {
+      return res.status(400).json({ message: 'Неверный пароль, попробуйте снова' });
+    }
+
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+  }
 });
 
 module.exports = router;
