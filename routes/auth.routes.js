01cc393b385fbcfb3b4e9a06104e6c870557fe4f
sandrig <sandrig.dev@gmail.com>
2019-12-29 19:41:20 +0300

    feat: add register user

---
+++ routes/auth.routes.js
@@ -0,0 +1,39 @@
+const { Router } = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const router = Router();
+
+// /api/auth/register
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const candidate = await User.findOne({ email });
+
+    if(candidate) {
+      return res.status(400).json({ message: 'Такой пользователь уже существует' })
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new User({ email, password: hashedPassword });
+
+    await user.save();
+
+    req.status(201).json({ message: 'Пользователь создан' })
+
+
+
+
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так, попробуйте снова' })
+  }
+});
+
+// /api/auth/login
+router.post('/login', async (req, res) => {
+
+});
+
+module.exports = router;
+
+
